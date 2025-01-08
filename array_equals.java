b4956c994299f81394ef2d005b49e81d094b3eab
aburehan87 <94138721+aburehan87@users.noreply.github.com>
2025-01-08 15:14:55 +0530

    Add files via upload

---
+++ array_equals.java
@@ -0,0 +1,12 @@
+
+import java.util.Arrays;
+
+public class array_equals {
+    public static void main(String[] args) {
+        String [] arr1 = { "r", "e", "h", "a", "n" };
+        String [] arr2 = {  "r", "e", "h", "a", "n" };
+
+        System.out.println(Arrays.equals(arr1, arr2));
+    }
+
+}
